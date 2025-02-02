cd45cd12fa0312c0ee354800ba3698e76f8481c5
Amir Norouzpour <Amirnorouzpour@live.com>
2025-02-02 16:40:27 +0100

    fix json field

--- src/core/components/fielddef/fields/json-field.tsx
+++ src/core/components/fielddef/fields/json-field.tsx
@@ -4,9 +4,22 @@ import { Form, Checkbox, Row, Col, InputNumber, Select, Input } from "antd";
 const JsonSettings: React.FC = () => {
   return (
     <Row gutter={16}>
-      <Col xs={24} md={24}>
-        <Form.Item name="className" label="Class name">
-          <Input placeholder="Namespace and Class name for deserialize" />
+      <Col xs={24} md={6}>
+        <Form.Item
+          name="showDataTypes"
+          label="Display Data Types"
+          valuePropName="checked"
+        >
+          <Checkbox></Checkbox>
+        </Form.Item>
+      </Col>
+      <Col xs={24} md={6}>
+        <Form.Item
+          name="showItemsSize"
+          label="Display Items Size"
+          valuePropName="checked"
+        >
+          <Checkbox></Checkbox>
         </Form.Item>
       </Col>
     </Row>
